4d76b9c258ea097a6ec64c1082152c7c8332e732
Tohasim <toreh.simonsen@live.dk>
2024-02-27 11:21:58 +0100

    working tictactoe game

--- script.js
+++ script.js
@@ -1,62 +1,105 @@
+window.addEventListener("load", start);
 
-window.addEventListener("load", start )
-
-//controller
-function start(){
-    console.log("javascript kører")
-    makeBoardClickable();
-    displayBoard();
-
-
-}
-function selectCell(row, col) {
-    writeToCell(row,col,1);
-    console.table(model);
-    displayBoard();
-
-}
-// view
-function makeBoardClickable() {
-    document.getElementById("board").addEventListener("click", boardClicked)
-
+// ******* CONTROLLER *********
+let currentPlayer = 1;
+function start() {
+	makeBoardClickable();
+	displayBoard();
 }
 
 function boardClicked(event) {
-    console.log("Board clicked!")
-    const cell = event.target;
-    const row = cell.dataset.row;
-    const col = cell.dataset.col;
-   console.log(`Clicked on Row: ${row} col: ${col}`);
-   selectCell(row, col);
-
+	const cell = event.target;
+	if (currentPlayer === 1 && cell.classList.contains("cell")) {
+		const row = cell.dataset.row;
+		const col = cell.dataset.col;
+		selectCell(row, col);
+	}
 }
-function displayBoard(){
-    for(let row = 0; row < 3; row++){
-        for(let col = 0; col < 3; col++){
-            const value = readFromCell(row,col);
-            console.log(value)
-            const cell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
-            cell.textContent = value;
-        }
-    }
-}
-// model
 
+function selectCell(row, col) {
+	if (readFromCell(row, col) === 0) {
+		writeToCell(row, col, currentPlayer);
+		displayBoard();
+		nextTurn();
+		return true;
+	}
+	return false;
+}
+
+function nextTurn() {
+	if (currentPlayer === 1) {
+		currentPlayer = 2;
+		setTimeout(computerTurn, 1000);
+	} else {
+		currentPlayer = 1;
+		playerTurn();
+	}
+}
+
+function playerTurn() {}
+
+function computerTurn() {
+	setTimeout(5000);
+	let availableCells = getEmptyCells();
+	if (availableCells.length === 0) {
+		alert("No more cells");
+	} else {
+		const index = Math.floor(Math.random() * availableCells.length);
+		const [row, col] = availableCells[index];
+		selectCell(row, col);
+	}
+}
+
+function getEmptyCells() {
+	let cells = [];
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			if (readFromCell(i, j) === 0) {
+				cells.push([i, j]);
+			}
+		}
+	}
+	return cells;
+}
+
+// ******* VIEW *********
+function makeBoardClickable() {
+	document.getElementById("board").addEventListener("click", boardClicked);
+}
+
+function displayBoard() {
+	for (let row = 0; row < 3; row++) {
+		for (let col = 0; col < 3; col++) {
+			const value = readFromCell(row, col);
+			const cell = document.querySelector(
+				`[data-row="${row}"][data-col="${col}"]`
+			);
+			switch (value) {
+				case 0:
+					cell.textContent = " ";
+					break;
+				case 1:
+					cell.textContent = "X";
+					break;
+				case 2:
+					cell.textContent = "O";
+					break;
+			}
+		}
+	}
+}
+
+// ******* MODEL *********
 const model = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0],
+	[0, 0, 0],
+	[0, 0, 0],
+	[0, 0, 0],
 ];
 
-function writeToCell(row, col, value){
-    model[row][col] = value
-
-
-
+function writeToCell(row, col, value) {
+	model[row][col] = value;
 }
 
-function readFromCell(row, col){
-    return  model[row][col]
-
+function readFromCell(row, col) {
+	return model[row][col];
 }
-
